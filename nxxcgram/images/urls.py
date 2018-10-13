81ae3af6f89f92bd76bf845eef62d0c5067d5f55
nxxc <nomad.nxxc@gmail.com>
2018-10-13 18:44:44 +0900

    Creating a comment / Deleting a comment only creator

--- nxxcgram/images/urls.py
+++ nxxcgram/images/urls.py
@@ -4,8 +4,9 @@ from . import views
 app_name = "images"
 urlpatterns = [
     path("", view=views.Feed.as_view(), name="feed"),
-    path("<image_id>/like/", view=views.LikeImage.as_view(), name="like_image"),
-    path("<image_id>/comment/", view=views.CommentOnImage.as_view(), name=" comment_image")
+    path("<image_id>/likes/", view=views.LikeImage.as_view(), name="like_image"),
+    path("<image_id>/comments/", view=views.CommentOnImage.as_view(), name="comment_image"),
+    path("comments/<comment_id>/", view=views.Comment.as_view(), name="comment")
 ]
 
 
