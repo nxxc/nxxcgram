b879830b60a8c6affc545e5c725de5ca68e55ac6
nxxc <nomad.nxxc@gmail.com>
2018-10-13 11:37:34 +0900

    Creating a comment / Deleting a comment

--- nxxcgram/images/urls.py
+++ nxxcgram/images/urls.py
@@ -4,7 +4,8 @@ from . import views
 app_name = "images"
 urlpatterns = [
     path("", view=views.Feed.as_view(), name="feed"),
-    path("<image_id>/like/", view=views.LikeImage.as_view(), name="like_image")
+    path("<image_id>/like/", view=views.LikeImage.as_view(), name="like_image"),
+    path("<image_id>/comment/", view=views.CommentOnImage.as_view(), name=" comment_image")
 ]
 
 
--- nxxcgram/images/views.py
+++ nxxcgram/images/views.py
@@ -1,5 +1,6 @@
-from rest_framework.views import APIView
 from rest_framework.response import Response
+from rest_framework.views import APIView
+from rest_framework import status
 from . import models, serializers
 
 
@@ -13,15 +14,15 @@ class Feed(APIView):
 
         image_list = []
 
-        for following_users in following_users:
+        for following_users in following_users:  # for following_users in following_users -> first "following_users"is  just variable
 
-            user_images = following_users.images.all()[:2]
+            user_images = following_users.images.all()[:2]  # images that is crated recently
 
             for image in user_images:
 
                 image_list.append(image)
 
-        sorted_list = sorted(image_list, key=lambda image: image.created_at, reverse=False)
+        sorted_list = sorted(image_list, key=lambda image: image.created_at, reverse=True)
 
         print(sorted_list)
         serializer = serializers.ImageSerializer(sorted_list, many=True)
@@ -33,6 +34,61 @@ class LikeImage(APIView):
 
     def get(self, request, image_id, format=None):
 
-        print(image_id)
+        user = request.user
 
-        return Response(status=200)
+        try:
+            found_image = models.Image.objects.get(id=image_id)
+
+        except models.Image.DoesNotExist:
+
+            return Response(status=status.HTTP_404_NOT_FOUND)
+
+        # try -> if the image has 'like' , run "return Response(status=status.HTTP_201_CREATED)" or run except ""
+
+        try:
+            preexisting_like = models.Like.objects.get(
+                creator=user,
+                image=found_image
+            )
+            preexisting_like.delete()
+
+            return Response(status=status.HTTP_204_NO_CONTENT)
+
+        except models.Like.DoesNotExist:
+
+            new_like = models.Like.objects.create(
+                creator=user,
+                image=found_image
+            )
+
+            # new_like.save()
+
+        # try -> Find preexisting_like on image -> if there is 'like' on image -> use preexisting_like.delete () to remove 'like' and return HTTP_204_NO_CONTENT
+        # except -> if there is no 'like' on image-> use models.Like.objects.create() to make 'like' -> return Response(status=status.HTTP_201_CREATED)
+
+        return Response(status=status.HTTP_201_CREATED)
+
+
+class CommentOnImage(APIView):
+
+    def post(self, request, image_id, format=None):
+
+        user = request.user  # the user who write a comment is user who request to HTTP
+        try:
+            found_image = models.Image.objects.get(id=image_id)
+
+        except models.Image.DoesNotExist:
+
+            return Response(status=status.HTTP_404_NOT_FOUND)
+
+        serializer = serializers.CommnetSerializer(data=request.data)
+
+        if serializer.is_valid():
+
+            serializer.save(creator=user, image=found_image)
+
+            return Response(data=serializer.data, status=status.HTTP_201_CREATED)
+
+        else:
+
+            return Response(data=serializer.errors, status=status.HTTP_400_BAD_REQUEST)
