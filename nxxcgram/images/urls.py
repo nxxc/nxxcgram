65165f0753678bbd46a58ecba430c7caafcb44ed
nxxc <nomad.nxxc@gmail.com>
2018-10-16 21:36:38 +0900

    Moderating comments on my images

--- nxxcgram/images/urls.py
+++ nxxcgram/images/urls.py
@@ -8,5 +8,6 @@ urlpatterns = [
     path('<image_id>/unlikes/', view=views.UnLikeImage.as_view(), name='unlike_image'),
     path('<image_id>/comments/', view=views.CommentOnImage.as_view(), name='comment_image'),
     path('comments/<comment_id>/', view=views.Comment.as_view(), name='comment'),
-    path('search/', view=views.Search.as_view(), name='search')
+    path('search/', view=views.Search.as_view(), name='search'),
+    path('<image_id>/comments/<comment_id>/', view=views.ModerateComments.as_view(), name='moderate_comments'),
 ]
