07364849ce5e92dd24798c7e86a1dfd471aebe83
nxxc <nomad.nxxc@gmail.com>
2018-10-16 23:53:51 +0900

    List Photo likes

--- nxxcgram/images/views.py
+++ nxxcgram/images/views.py
@@ -3,7 +3,8 @@ from rest_framework.views import APIView
 from rest_framework import status
 from . import models, serializers
 from nxxcgram.notifications import views as notification_views
-
+from nxxcgram.users import models as user_models
+from nxxcgram.users import serializers as user_serializers
 
 class Feed(APIView):
 
@@ -40,6 +41,18 @@ class Feed(APIView):
 class LikeImage(APIView):
 
     def get(self, request, image_id, format=None):
+    
+        likes = models.Like.objects.filter(image__id=image_id)
+
+        like_creators_ids = likes.values('creator_id')
+
+        users = user_models.User.objects.filter(id__in=like_creators_ids)
+
+        serializer = user_serializers.ListUserSerializer(users, many=True)
+
+        return Response(data=serializer.data, status=status.HTTP_200_OK)
+
+    def post(self, request, image_id, format=None):
 
         user = request.user
 
