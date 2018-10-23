0de5c95954005dccf3f1b1d5799c1562b623d04b
nxxc <nomad.nxxc@gmail.com>
2018-10-23 15:49:48 +0900

    Syncing React Router with Redux/Debugging Redux like a Pro with Reactotron

--- frontend/src/redux/configureStore.js
+++ frontend/src/redux/configureStore.js
@@ -1,23 +1,33 @@
 import { createStore, combineReducers, applyMiddleware } from "redux";
 import thunk from "redux-thunk";
+import { routerReducer, routerMiddleware } from "react-router-redux";
+import createHistory from "history/createBrowserHistory";
 import users from "redux/modules/users";
+import Reactotron from "ReactotronConfig";
 
 const env = process.env.NODE_ENV;
 
-const middlewares = [thunk];
+const history = createHistory();
+
+const middlewares = [thunk, routerMiddleware(history)];
 
 if (env === "development") {
   const { logger } = require("redux-logger");
   middlewares.push(logger);
 }
 
-console.log(env);
-
 const reducer = combineReducers({
-  users
+  users,
+  routing: routerReducer
 });
 
-let store = initialState =>
-  createStore(reducer, applyMiddleware(...middlewares));
+let store;
 
+if (env === "development") {
+  store = initialState =>
+    Reactotron.createStore(reducer, applyMiddleware(...middlewares));
+} else {
+  store = initialState => createStore(reducer, applyMiddleware(...middlewares));
+}
+export { history };
 export default store();
