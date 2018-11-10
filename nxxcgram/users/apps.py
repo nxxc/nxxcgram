73d285d4a4a4c7461733510a350f0d12a5da87f6
nxxc <nomad.nxxc@gmail.com>
2018-11-10 22:54:14 +0900

    Getting Extra information from Facebook Login

--- nxxcgram/users/apps.py
+++ nxxcgram/users/apps.py
@@ -2,12 +2,13 @@ from django.apps import AppConfig
 
 
 class UsersAppConfig(AppConfig):
-
-    name = "nxxcgram.users"
+    name = 'nxxcgram.users'
     verbose_name = "Users"
 
     def ready(self):
-        try:
-            import users.signals  # noqa F401
-        except ImportError:
-            pass
+        """Override this to put in:
+            Users system checks
+            Users signal registration
+        """
+
+        from .signals import user_signed_up
